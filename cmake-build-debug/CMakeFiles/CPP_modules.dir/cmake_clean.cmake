612a832eab94f38bd1044b48dfb0b4a0b10d8070
lPrAYl <prokopenko_andrey@mail.ru>
2022-01-10 15:33:26 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/CPP_modules.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_modules.dir/main.cpp.o"
+  "CPP_modules"
+  "CPP_modules.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_modules.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
